818cfb5994d7a326a49af261067aa4fbcdc56098
Fouad Karam <11132011+Fouad-Karam@users.noreply.github.com>
2021-12-15 00:11:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import pandas
 import random
-from tkinter import messagebox
+# from tkinter import messagebox
 
 # ******************** Functions *********************
 data = pandas.read_csv("data/multiplication.csv")
@@ -10,14 +10,11 @@ current_card = {}
 
 
 def next_card():
-    try:
-        global current_card
-        current_card = random.choice(to_learn)
-        canvas.itemconfig(card_title, text="Table")
-        canvas.itemconfig(card_word, text=current_card["Table"])
-        canvas.itemconfig(card_background, image=card_front_img)
-    except IndexError:
-        messagebox.showwarning(title="Warning", message="You have answered all the questions. Please close the app")
+    global current_card
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="Table")
+    canvas.itemconfig(card_word, text=current_card["Table"])
+    canvas.itemconfig(card_background, image=card_front_img)
 
 
 def answer_card():
@@ -33,15 +30,10 @@ def wrong_card():
 
 
 def is_known():
-    try:
-        to_learn.remove(current_card)
-        # print(len(to_learn))
-        canvas.itemconfig(card_title, text="Correct! Next Q")
-        # canvas.itemconfig(card_word, text="Next Q")
-        # canvas.itemconfig(card_background, image=card_back_img)
-    except ValueError:
-        messagebox.showwarning(title="Oops!",
-                               message="You clicked the green check mark already. Pick the next question.")
+    to_learn.remove(current_card)
+    canvas.itemconfig(card_title, text="Correct!")
+    canvas.itemconfig(card_word, text="Next Q")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 # ******************** UI *********************
@@ -57,7 +49,7 @@ card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Multiplication", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="Are you Ready?", font=("Ariel", 60, "bold"))
+card_word = canvas.create_text(400, 263, text="Are you Ready?", font=("Ariel", 40, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=4)
@@ -78,6 +70,5 @@ right_img = PhotoImage(file="images/right.png")
 known_button = Button(image=right_img, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=3)
 
-# next_card()
 
 window.mainloop()
